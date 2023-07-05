b7d9c4186fa897ae0b2f55f6b98077e4ec588100
rafaeltedesco <dev.rafaeltedesco@gmail.com>
2023-07-05 07:32:03 -0300

    :sparkles: jwt implemented

--- src/main/java/com/courses/courselms/services/JwtService.java
+++ src/main/java/com/courses/courselms/services/JwtService.java
@@ -3,6 +3,7 @@ package com.courses.courselms.services;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTCreationException;
+import com.auth0.jwt.exceptions.JWTVerificationException;
 import com.courses.courselms.models.User;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
@@ -16,14 +17,13 @@ public class JwtService {
 
     @Value("${api.security.token.secret}")
     private String secret;
+    private final String issuer = "course lms";
 
     public String generateToken(User user) {
         try {
-
-            System.out.println(secret);
-            var algorithm = Algorithm.HMAC256(secret);
+            Algorithm algorithm = Algorithm.HMAC256(secret);
             return JWT.create()
-                    .withIssuer("course lms")
+                    .withIssuer(issuer)
                     .withSubject(user.getUsername())
                     .withClaim("id", user.getId())
                     .withExpiresAt(Instant.now().plus(Duration.ofMinutes(10)))
@@ -31,6 +31,19 @@ public class JwtService {
         } catch (JWTCreationException ex) {
             throw new RuntimeException("Error while generating JWT", ex);
         }
+    }
 
+
+    public String getSubject(String token) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256(secret);
+            return JWT.require(algorithm)
+                    .withIssuer(issuer)
+                    .build()
+                    .verify(token)
+                    .getSubject();
+        } catch (JWTVerificationException ex) {
+            throw new RuntimeException("Token inválido!");
+        }
     }
 }
